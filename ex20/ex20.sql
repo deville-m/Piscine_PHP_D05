1c6bbee648ea3bd5c333bf56e3c9d46f47e9f8fa
Matthew DEVILLE <mdeville@e2r6p19.42.fr>
2018-04-03 22:32:46 +0200

    help

--- ex20/ex20.sql
+++ ex20/ex20.sql
@@ -1,4 +1,5 @@
-SELECT id_genre, title, id_distrib, distrib.name
+SELECT film.id_genre as "id_genre", genre.name as "name_genre", film.id_distrib as "id_distrib", distrib.name as "name_distrib", title as "title_film"
 FROM film
-INNER JOIN distrib ON film.id_distrib == distrib.id_distrib
-WHERE 
+LEFT JOIN distrib ON film.id_distrib = distrib.id_distrib
+LEFT JOIN genre ON film.id_genre = genre.id_genre
+WHERE film.id_genre >= 4 AND film.id_genre <= 8;
